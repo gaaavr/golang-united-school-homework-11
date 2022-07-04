5354e07015f3c77a2a086ec2efbd4cfffbc74853
Гаврилин Сергей Сергеевич <s.gavrilin@usr.vipholding.local>
2022-07-04 11:07:10 +0300

    semaphore

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,26 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var (
+		wg sync.WaitGroup
+		mu sync.Mutex
+	)
+	sem := make(chan struct{}, pool)
+	for i := int64(0); i < n; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(j int64) {
+			defer wg.Done()
+			defer func() {
+				<-sem
+			}()
+			u := getOne(j)
+			mu.Lock()
+			res = append(res, u)
+			mu.Unlock()
+		}(i)
+	}
+
+	wg.Wait()
+	return res
 }
